e4c4df79e416a9a0b3b7848901cf9f71dd207608
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2025-12-18 00:37:39 -0300

    added calculation for next month available money, needs refactor for current month available money

--- personal_finance_analytics/domain/expenses.py
+++ personal_finance_analytics/domain/expenses.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 
 from .months import get_months_until_now, get_next_month
+from .salaries import get_last_net_salary
 
 SPREADSHEET_ID = os.getenv("EXPENSES_SPREADHSEET_ID")
 NEXT_YEAR_EXPENSES_SPREADHSEET_ID = os.getenv("NEXT_YEAR_EXPENSES_SPREADHSEET_ID")
@@ -10,6 +11,17 @@ EXPENSES_GROUPS = {
     "fijos": ["alquileres", "servicios_esenciales", "servicios_no_esenciales"],
     "corrientes": ["hogar", "transporte", "salidas"],
     "irregulares": ["shopping", "otros"],
+}
+
+MAXIMUM_PERCENTAGES_PER_CATEGORY = {
+    "alquileres": 30,
+    "servicios_esenciales": 7,
+    "servicios_no_esenciales": 5,
+    "hogar": 15,
+    "transporte": 5,
+    "salidas": 8,
+    "shopping": 7,
+    "otros": 3,
 }
 
 
@@ -65,3 +77,27 @@ def get_next_month_expenses_dataframe() -> pd.DataFrame:
     )
     months_totals.append(_calculate_month_totals(next_month, df))
     return pd.DataFrame(months_totals).set_index("mes")
+
+
+def get_next_month_available_money_per_category():
+    """Uses get_next_month_expenses_dataframe as well as get_last_net_salary to calculate available money per category for next month, based on the specified maximum percentages per categories"""
+    next_month_expenses_df = get_next_month_expenses_dataframe()
+    last_net_salary = get_last_net_salary()
+
+    available_money_per_category = {}
+    for category, max_percentage in MAXIMUM_PERCENTAGES_PER_CATEGORY.items():
+        max_amount_for_category = (max_percentage / 100) * last_net_salary
+        planned_expense = next_month_expenses_df.at[
+            next_month_expenses_df.index[0], category
+        ]
+        available_money_per_category[category] = float(
+            max_amount_for_category - planned_expense
+        )
+
+    return available_money_per_category
+
+
+# available = get_next_month_available_money_per_category()
+
+# for k, v in available.items():
+#     print(f"Available money for {k} in next month: ${v:,.2f}")
--- personal_finance_analytics/domain/salaries.py
+++ personal_finance_analytics/domain/salaries.py
@@ -6,3 +6,9 @@ def get_salaries_csv() -> pd.DataFrame:
     return pd.read_csv(  # type: ignore
         f"https://docs.google.com/spreadsheets/d/{os.getenv('SALARIES_SPREADHSEET_ID')}/gviz/tq?tqx=out:csv&sheet=2020"
     )
+
+
+def get_last_net_salary() -> float:
+    df = get_salaries_csv()
+    last_salary = df["sueldo_neto_ars"].iloc[-1]
+    return float(last_salary)

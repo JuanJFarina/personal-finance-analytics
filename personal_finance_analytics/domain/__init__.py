cfbd0beea4cfcf7720e988e4513900249be695b8
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2025-12-24 16:59:15 -0300

    remove leftover import

--- personal_finance_analytics/domain/__init__.py
+++ personal_finance_analytics/domain/__init__.py
@@ -1,6 +1,5 @@
 from .exceptions import ExpensesSpreadsheetException, SalariesSpreadsheetException
 from .entities import AvailableFunds
-from .expenses import get_current_month_available_money_per_category
 from .salaries import get_last_adjusted_salary, get_last_net_salary
 from .finance_analyst import FinanceAnalyst
 
@@ -8,7 +7,6 @@ __all__ = [
     "ExpensesSpreadsheetException",
     "SalariesSpreadsheetException",
     "AvailableFunds",
-    "get_current_month_available_money_per_category",
     "get_last_adjusted_salary",
     "get_last_net_salary",
     "FinanceAnalyst",
